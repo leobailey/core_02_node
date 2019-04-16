c36ec19768a1afd77dfe1c4564f2beb5224cc807
Leo Bailey <leobailey@hotmail.com>
2019-04-16 13:36:25 +0100

    remove express stuff

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ var express = require('express');
 
 var app = express();
 
-var port = process.env.PORT || 3000;
+// var port = process.env.PORT || 3000;
 
-app.get('/', (req, res) =>{
-    res.send('Welcome to my API');
-});
+// app.get('/', (req, res) =>{
+//     res.send('Welcome to my API');
+// });
 
-app.listen(port, ()=>{
-    console.log('Running on port ' + port);
-});
+// app.listen(port, ()=>{
+//     console.log('Running on port ' + port);
+// });
